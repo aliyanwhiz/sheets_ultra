ae1714dfcd6c3eba0b50940137766e95f14391ac
aliyanwhiz <aliyanahmed853@gmail.com>
2024-04-22 10:43:44 +0500

    new ultra

--- assets/js/overtime_sheet.js
+++ assets/js/overtime_sheet.js
@@ -1,65 +1,4 @@
-const pattern = [
-    {
-        month : "january",
-        days : 31,
-    },
-    {
-        month : "february",
-        days : 28,
-    },
-    {
-        month : "march",
-        days : 31,
-    },
-    {
-        month : "april",
-        days : 30,
-    },
-    {
-        month : "may",
-        days : 31,
-    },
-    {
-        month : "june",
-        days : 30,
-    },
-    {
-        month : "july",
-        days : 31,
-    },
-    {
-        month : "august",
-        days : 31,
-    },
-    {
-        month : "september",
-        days : 30,
-    },
-    {
-        month : "october",
-        days : 31,
-    },
-    {
-        month : "november",
-        days : 30,
-    },
-    {
-        month : "december",
-        days : 31,
-    },
-]
 let suffix = ["pre.","off day","Gaz. day","O.D. Pre.","G.D. Pre.","Paid lv.", "Abs.","Un-paid","Paid Days","Tot. Days"  ]
-
-
-    const month = new Date().getMonth();
-
-for (let i = 0; i < pattern.length; i++) {
-    if(i === month)
-    {
-        console.log(pattern[i].month, pattern[i].days);
-    }
-    
-}
     
 let year = new Date().getFullYear()
 
@@ -77,6 +16,8 @@ function dateHeaders() {
     document.getElementById("strtdate").innerHTML = `${firstDateOfMonth.getDate()}-${firstDateOfMonth.toLocaleString('default', { month: 'short' })}-${firstDateOfMonth.getFullYear()}`;
     document.getElementById("strtdate2").innerHTML = `${firstDateOfMonth.getDate()}-${firstDateOfMonth.toLocaleString('default', { month: 'short' })}-${firstDateOfMonth.getFullYear()}`;
     document.getElementById("enddate").innerHTML = `${lastDateOfMonth.getDate()}-${lastDateOfMonth.toLocaleString('default', { month: 'short' })}-${lastDateOfMonth.getFullYear()}`;
+    document.getElementById("enddate2").innerHTML = `${lastDateOfMonth.getDate()}-${lastDateOfMonth.toLocaleString('default', { month: 'short' })}-${lastDateOfMonth.getFullYear()}`;
+    document.getElementById("enddate3").innerHTML = `${lastDateOfMonth.getDate()}-${lastDateOfMonth.toLocaleString('default', { month: 'short' })}-${lastDateOfMonth.getFullYear()}`;
     for (let i = 1; i <= lastDateOfMonth.getDate(); i++) {
         makeDateHeaders(new Date(year, month, i)
                                 .toLocaleString('default', 
